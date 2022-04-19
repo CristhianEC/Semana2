5ab4cdde6884cc650f543175b2b30a2f55d16403
Cristhian <motochecristhian@gmail.com>
2022-04-18 21:30:29 -0500

    Agregar archivos de proyecto.

---
+++ S29AClaseMA/Vistas/VistaDos.xaml.cs
@@ -0,0 +1,45 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S29AClaseMA
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VistaDos : ContentPage
+    {
+        public VistaDos()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+
+            try
+            {
+                double numero1 = Convert.ToDouble(txtNumero1.Text);
+
+                double numero2 = Convert.ToDouble(txtNumero2.Text);
+
+                double numero3 = Convert.ToDouble(txtNumero3.Text);
+
+                double respuesta = numero1 + numero2 + numero3;
+
+                txtResultado.Text = respuesta.ToString();
+
+                lblResultado.Text = respuesta.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("ERROR", ex.Message, "Cerrar");
+            }
+
+        }
+    }
+}
